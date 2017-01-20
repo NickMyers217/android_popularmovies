3a56346a5703a8b94ff6ddbb5fe0a1d93a992adc
Nickmyers217 <nickbmyers217@gmail.com>
2017-01-19 18:25:27 -0600

    chore: Setup and tested the Picasso image library

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO (1): get the image library working
+        Picasso.with(this)
+                .load("http://i.imgur.com/DvpvklR.png")
+                .into((ImageView) findViewById(R.id.iv_test_image));
+        // TODO (2): get the api url's working
+        // TODO (3): get network requests working
     }
 }
